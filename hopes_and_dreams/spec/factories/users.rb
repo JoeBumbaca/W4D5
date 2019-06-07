5ade3ab1c06c6e7811d48b438740aa4a51febb8c
JoeBumbaca <joebumbaca@gmail.com>
2019-06-07 14:46:14 -0700

    implement some spec tests in user model

--- hopes_and_dreams/spec/factories/users.rb
+++ hopes_and_dreams/spec/factories/users.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :user do
-    
+    username { Faker::Name.name }
+    password {'password123'}
+  end
+
+  factory :bob do
+    username { 'Bob' }
+    password { 'password123' }
   end
 end
--- hopes_and_dreams/spec/models/user_spec.rb
+++ hopes_and_dreams/spec/models/user_spec.rb
@@ -1,13 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
 
   it { should validate_presence_of(:username) }
   it { should validate_presence_of(:password_digest) }
   it { should validate_uniqueness_of(:username) }
+  subject(:fred) { User.create(
+      username: 'fred', 
+      password: 'password'
+    ) 
+  }
   
-  # association
-  it { should have_many(:goals) }
+  describe 'associations' do 
+    it { should have_many(:goals) }
+  end
+
+  describe "self.find_by_credentials" do 
+    before (:each) do  
+      FactoryBot.create(:bob)
+    end
+
+    context "with valid credentials" do 
+      it "should find the correct user" do
+        bob = User.find_by_credentials(username: 'Bob', password: 'password123')
+        expect(bob.username).to eq(User.last.username)
+      end
+    end
+
+    context "with invalid credentials" do 
+      it "should return nil" do
+        invalid_user = User.find_by_credentials(username: 'Steve', password: 'password123')
+        expect(invalid_user).to be nil
+      end
+    end
+  end
 
 end
