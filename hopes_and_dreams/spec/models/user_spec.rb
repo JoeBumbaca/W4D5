59df4424e74336b4008c907f6e8a009172609f28
JoeBumbaca <joebumbaca@gmail.com>
2019-06-07 12:29:00 -0700

    lunch break

--- hopes_and_dreams/spec/models/user_spec.rb
+++ hopes_and_dreams/spec/models/user_spec.rb
@@ -4,7 +4,10 @@ RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 
   it { should validate_presence_of(:username) }
-  it { should validate_presence_of(:password) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_uniqueness_of(:username) }
   
+  # association
+  it { should have_many(:goals) }
 
 end
