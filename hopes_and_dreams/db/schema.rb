e13ac8c385b1093ec7059b71a42697b2f283bdab
JoeBumbaca <joebumbaca@gmail.com>
2019-06-07 12:19:09 -0700

    updated password column in users table

--- hopes_and_dreams/db/schema.rb
+++ hopes_and_dreams/db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_07_185300) do
+ActiveRecord::Schema.define(version: 2019_06_07_191633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
-    t.string "password", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest", null: false
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
