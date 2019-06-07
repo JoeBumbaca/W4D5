dea6f87556f5ba6a7204725f33bea324e4cd0f06
JoeBumbaca <joebumbaca@gmail.com>
2019-06-07 16:16:57 -0700

    Complete Users controller

--- hopes_and_dreams/app/controllers/users_controller.rb
+++ hopes_and_dreams/app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+    #We are signing up a user
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
 end
